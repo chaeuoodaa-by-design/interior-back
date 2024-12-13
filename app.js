46df9f4fe8fa839afac5f024c88444c787de6f26
zv322 <dhssnfl258@naver.com>
2024-12-13 19:37:43 +0900

    lambda cicd

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+/**
+* Lambda Endpoint -> /api/{proxy+}
+*
+* */
+
+
 app.get('/api/test', (req, res) => {
-    res.json({ message: 'Hello from Lambda!!!!' });
+    res.json({ message: '무림맹별동대' });
 });
 // 나머지 경로 처리
 app.all('*', (req, res) => {
