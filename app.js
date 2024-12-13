4235e1b269711b3dbaf38fc1ad10f18b316856f7
zv322 <dhssnfl258@naver.com>
2024-12-13 19:29:44 +0900

    lambda tester

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const app = express();
 
-// 미들웨어 설정
-app.use(express.json());
-
-// 라우트 설정
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello from Lambda!' });
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Hello from Lambda!!!!' });
 });
-
-app.get('/users', (req, res) => {
-    res.json({ users: [{ id: 1, name: 'Tester' }] });
-});
-
 // 나머지 경로 처리
 app.all('*', (req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
-
 module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./index');
+const app = require('./app');
+
+// Express 애플리케이션으로 AWS Lambda 서버 생성
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
+    // Lambda 요청을 Express로 전달
     awsServerlessExpress.proxy(server, event, context);
 };
